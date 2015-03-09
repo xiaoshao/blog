50b502fc2cf93d1cd616b889edec5fa7b6957972
Jichao Ouyang <oyanglulu@gmail.com>
2015-03-09 10:50:39 +0800

    update import

--- import.sh
+++ import.sh
@@ -1,8 +1,13 @@
-#!/bin/bash
-for file in $(find blog-master -iname "*.md")
+#!/bin/bash -e
+
+for file in $(find org -iname "*.md")
   do
-  	pandoc $file -f markdown -t org -o ${file}.org
-    cp ${file}.org org/
+      pandoc $file -f markdown -t org -o ${file}.org
+      touch -r $file ${file}.org
+      echo "convert markdown $file into org"
 done
 
-cp -r blog-master/org/* org/
+ls -al org/
+ls -al org/markdown/
+
+
