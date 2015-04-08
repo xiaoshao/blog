964c9da46bf3ebfd9da5c1fa985c03835825f052
Jichao Ouyang <oyanglulu@gmail.com>
2015-04-08 16:30:43 +0800

    add setup step to readme

--- emacs.el
+++ emacs.el
@@ -29,7 +29,7 @@
         :base-directory ,blog-path
         :base-extension "org"
         :html-doctype "html5"
-        :html-head "<meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"/style/main.css\" /><link rel=\"alternate\" type=\"application/rss+xml\" title=\"ResiWorks\" href=\"http://www.yoursite.com/index.xml\">"
+        :html-head "<meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><link rel=\"stylesheet\" href=\"style/main.css\" /><link rel=\"alternate\" type=\"application/rss+xml\" title=\"ResiWorks\" href=\"http://www.yoursite.com/index.xml\">"
         :html-html5-fancy t
         :html-postamble ,postamble
         ;; HTML directory
